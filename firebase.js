fd5bcfae1702e44d9bf8fd5e26db22bfbb444419
Warley Oliveira <softwarley@hotmail.com>
2026-05-27 08:35:25 -0300

    fix firebase3

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAbgUsk2l2DQ-Ds9YU_qeTvOVIwIeiMTlo",
   authDomain: "konta-base.firebaseapp.com",
   projectId: "konta-base",
@@ -8,5 +8,3 @@ const firebaseConfig = {
   appId: "1:922550726067:web:376ca0bd2871f22d5a2fc6",
   measurementId: "G-N7CJEE3MHP",
 };
-
-module.exports = { firebaseConfig };
